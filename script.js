f82840f704f0e9c6f6569bc4247b0b43d6036c1b
Jessica Twitmyer <jatwitmyer@gmail.com>
2023-08-14 11:43:41 -0400

    Adds all locations and connects them to one another

--- script.js
+++ script.js
@@ -37,25 +37,84 @@ const bridge = {
     text1: "text",
     text2: "text",
 }
-
-const firefly = "https://aiartshop.com/cdn/shop/products/magical-forest-01-ai-painting-917.webp?v=1677621643";
-const fireflyTree = "https://www.bhmpics.com/downloads/enchanted-forest-desktop-wallpaper-/25.1582033499_enchanted-forest_vsthemes_ru-21.webp";
-const altFireflyTree = "https://e1.pxfuel.com/desktop-wallpaper/614/830/desktop-wallpaper-enchanted-forest-enchanted-forest-backgrounds-mystical-forest.jpg";
-
-const upstream = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0dce719a-cfab-4cbd-b5f0-6305626028e6/d6u1xef-2904418a-6874-44c6-a252-c06e7ec9bc6d.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBkY2U3MTlhLWNmYWItNGNiZC1iNWYwLTYzMDU2MjYwMjhlNlwvZDZ1MXhlZi0yOTA0NDE4YS02ODc0LTQ0YzYtYTI1Mi1jMDZlN2VjOWJjNmQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.1JFOYPHMUdvnp8uxEjXE3Mo7AvXll8mPq7m46oXZOEU";
-const waterfall = "https://w0.peakpx.com/wallpaper/700/796/HD-wallpaper-fantasy-forest-forest-colorful-lovely-view-colors-beautiful-magic-trees-swamp-valley-warrior-water-splendor-green-men.jpg";
-const treeHouse = "https://images.nightcafe.studio/jobs/wi0XlQDMXSgsQywQQ0FE/wi0XlQDMXSgsQywQQ0FE--2--g8ety.jpg?tr=w-1600,c-at_max";
-
-const downstream = "https://as1.ftcdn.net/v2/jpg/05/47/03/72/1000_F_547037240_LUekRoVId3q55j70Spyea0K6SdsJjzFo.jpg";
-const downstream2 = "https://as2.ftcdn.net/v2/jpg/05/73/05/69/1000_F_573056930_NCI3XfYl3vaJQKiqZXtTmPy77z7RGK8y.jpg";
-
-const swamp1 = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0975fc90-2a41-4206-9525-18338e368074/d966lr9-1f0cab17-f5df-4b5d-a95a-1ecd07806dcd.jpg/v1/fit/w_828,h_606,q_70,strp/swamp_by_adampaquette_d966lr9-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODA0IiwicGF0aCI6IlwvZlwvMDk3NWZjOTAtMmE0MS00MjA2LTk1MjUtMTgzMzhlMzY4MDc0XC9kOTY2bHI5LTFmMGNhYjE3LWY1ZGYtNGI1ZC1hOTVhLTFlY2QwNzgwNmRjZC5qcGciLCJ3aWR0aCI6Ijw9MTEwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.D2qN6t3k8SMJF2bjbg2DPJiqdaed_-TVpPKtap7wfOg";
-const swamp2 = "https://i.pinimg.com/originals/16/f2/77/16f277499b1b23f53372815889c15e00.jpg";
-const swampVillage = "https://e0.pxfuel.com/wallpapers/609/778/desktop-wallpaper-%E2%9C%BC-lanterns-in-the-swamp-%E2%9C%BC-lanterns-colors-scenery-trees-jungle-lamps-attractions-in-dreams-forests-paintings-swamp-beautiful-fireflies-creative-pre-made-love-four-seasons-landscapes.jpg";
-const swampCircle = "https://images.nightcafe.studio/jobs/8qWAPhnH1CBqk9pBaFh1/8qWAPhnH1CBqk9pBaFh1--2--9kc8q_2x.jpg?tr=w-1600,c-at_max";
-const swampRuins = "https://images.nightcafe.studio/jobs/pWlflDYguTfYUdbhhKjN/pWlflDYguTfYUdbhhKjN--1--jwcey_2x.jpg?tr=w-1600,c-at_max";
-
-
+const firefly = {
+    visited: false,
+    img: "https://aiartshop.com/cdn/shop/products/magical-forest-01-ai-painting-917.webp?v=1677621643",
+    text1: "text",
+    text2: "text",
+}
+const fireflyTree = {
+    visited: false,
+    img: "https://www.bhmpics.com/downloads/enchanted-forest-desktop-wallpaper-/25.1582033499_enchanted-forest_vsthemes_ru-21.webp",
+    text1: "text",
+    text2: "text",
+}
+const altFireflyTree = {
+    visited: false,
+    img: "https://e1.pxfuel.com/desktop-wallpaper/614/830/desktop-wallpaper-enchanted-forest-enchanted-forest-backgrounds-mystical-forest.jpg",
+    text1: "text",
+    text2: "text",
+}
+const upstream = {
+    visited: false,
+    img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0dce719a-cfab-4cbd-b5f0-6305626028e6/d6u1xef-2904418a-6874-44c6-a252-c06e7ec9bc6d.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBkY2U3MTlhLWNmYWItNGNiZC1iNWYwLTYzMDU2MjYwMjhlNlwvZDZ1MXhlZi0yOTA0NDE4YS02ODc0LTQ0YzYtYTI1Mi1jMDZlN2VjOWJjNmQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.1JFOYPHMUdvnp8uxEjXE3Mo7AvXll8mPq7m46oXZOEU",
+    text1: "text",
+    text2: "text",
+}
+const waterfall = {
+    visited: false,
+    img: "https://w0.peakpx.com/wallpaper/700/796/HD-wallpaper-fantasy-forest-forest-colorful-lovely-view-colors-beautiful-magic-trees-swamp-valley-warrior-water-splendor-green-men.jpg",
+    text1: "text",
+    text2: "text",
+}
+const treeHouse = {
+    visited: false,
+    img: "https://images.nightcafe.studio/jobs/wi0XlQDMXSgsQywQQ0FE/wi0XlQDMXSgsQywQQ0FE--2--g8ety.jpg?tr=w-1600,c-at_max",
+    text1: "text",
+    text2: "text",
+}
+const downstream1 = {
+    visited: false,
+    img: "https://as1.ftcdn.net/v2/jpg/05/47/03/72/1000_F_547037240_LUekRoVId3q55j70Spyea0K6SdsJjzFo.jpg",
+    text1: "text",
+    text2: "text",
+}
+const dam = {
+    visited: false,
+    img: "https://as2.ftcdn.net/v2/jpg/05/73/05/69/1000_F_573056930_NCI3XfYl3vaJQKiqZXtTmPy77z7RGK8y.jpg",
+    text1: "text",
+    text2: "text",
+}
+const swampLost = {
+    visited: false,
+    img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0975fc90-2a41-4206-9525-18338e368074/d966lr9-1f0cab17-f5df-4b5d-a95a-1ecd07806dcd.jpg/v1/fit/w_828,h_606,q_70,strp/swamp_by_adampaquette_d966lr9-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODA0IiwicGF0aCI6IlwvZlwvMDk3NWZjOTAtMmE0MS00MjA2LTk1MjUtMTgzMzhlMzY4MDc0XC9kOTY2bHI5LTFmMGNhYjE3LWY1ZGYtNGI1ZC1hOTVhLTFlY2QwNzgwNmRjZC5qcGciLCJ3aWR0aCI6Ijw9MTEwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.D2qN6t3k8SMJF2bjbg2DPJiqdaed_-TVpPKtap7wfOg",
+    text1: "text",
+    text2: "text",
+}
+const beyondDam = {
+    visited: false,
+    img: "https://i.pinimg.com/originals/16/f2/77/16f277499b1b23f53372815889c15e00.jpg",
+    text1: "text",
+    text2: "text",
+}
+const swampVillage = {
+    visited: false,
+    img: "https://e0.pxfuel.com/wallpapers/609/778/desktop-wallpaper-%E2%9C%BC-lanterns-in-the-swamp-%E2%9C%BC-lanterns-colors-scenery-trees-jungle-lamps-attractions-in-dreams-forests-paintings-swamp-beautiful-fireflies-creative-pre-made-love-four-seasons-landscapes.jpg",
+    text1: "text",
+    text2: "text",
+}
+const swampCircle = {
+    visited: false,
+    img: "https://images.nightcafe.studio/jobs/8qWAPhnH1CBqk9pBaFh1/8qWAPhnH1CBqk9pBaFh1--2--9kc8q_2x.jpg?tr=w-1600,c-at_max",
+    text1: "text",
+    text2: "text",
+}
+const swampRuins = {
+    visited: false,
+    img: "https://images.nightcafe.studio/jobs/pWlflDYguTfYUdbhhKjN/pWlflDYguTfYUdbhhKjN--1--jwcey_2x.jpg?tr=w-1600,c-at_max",
+    text1: "text",
+    text2: "text",
+}
 
 //Updates all places with their connections
 startingPath.forward = portal
@@ -73,7 +132,36 @@ coveredPath.forward = treeVillage
 treeVillage.backward = coveredPath
 
 bridge.backward = forkedPath
+bridge.forward = firefly
+bridge.left = downstream1
+bridge.right = upstream
 
+firefly.backward = bridge
+firefly.forward = fireflyTree
+
+fireflyTree.backward = firefly
+
+downstream1.backward = bridge
+downstream1.forward = dam
+downstream1.right = swampLost
+
+dam.backward = downstream1
+dam.forward = beyondDam
+dam.right = swampVillage
+
+swampVillage.backward = dam
+
+beyondDam.backward = dam
+
+swampLost.backward = downstream1
+
+upstream.backward = bridge
+upstream.forward = waterfall
+
+waterfall.backward = upstream
+waterfall.forward = treeHouse
+
+treeHouse.backward = waterfall
 
 function changeImgTo (newImg) {
     const oldImg = document.getElementById("view");
