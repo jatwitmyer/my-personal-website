fa5f3995e654a418f9e0395a58410b47036e8229
Jessica Twitmyer <jatwitmyer@gmail.com>
2023-08-14 06:15:07 -0400

    Adds starting view and description to index.html and style.css, adds all location links to scripts.js, adds changeViewTo function for changing the onscreen image in scripts.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-My Personal Website
+This is a choose your own adventure game set in an enchanted forest.
+
+The credits for all art used will be added at the bottom of the screen on the page where it's used.
--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+// const h2 = document.createElement("h2");
+// h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+// document.querySelector("body").appendChild(h2);
+
+function changeViewTo (newView) {
+    const oldView = document.getElementById("view");
+    oldView.src = newView;
+}
+
+const firstPath = "https://img.freepik.com/premium-photo/old-green-edge-forest-humidity-moss-trees-path-leading-through-dense-fairytale-forest_86390-10375.jpg";
+const portal = "https://as1.ftcdn.net/v2/jpg/05/40/95/08/1000_F_540950809_3FWosaP1ueYJ6QVla5SfdaAGMTPwU9gC.jpg"
+const forkedPath = "https://jkroots.com/wp-content/uploads/2018/10/enchanted_forest_2_by_jkroots-daxl6xh.jpg";
+
+const coveredPath = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/65282199-9f85-4daa-bcd6-de2f615bcca7/dfmb909-a1146529-df3c-4ad2-885f-89c56c667358.png/v1/fit/w_828,h_828,q_70,strp/path_through_an_enchanted_forest_version_1_by_pm_artistic_dfmb909-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTI4MCIsInBhdGgiOiJcL2ZcLzY1MjgyMTk5LTlmODUtNGRhYS1iY2Q2LWRlMmY2MTViY2NhN1wvZGZtYjkwOS1hMTE0NjUyOS1kZjNjLTRhZDItODg1Zi04OWM1NmM2NjczNTgucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.60xIH9oaHILuKa4qbFpYtgunL2htU-llQmqB6hEhf6k";
+const treeVillage = "https://wallpapers.com/images/hd/mystical-forest-1920-x-1200-zggop07wd2gh8vmg.jpg";
+
+const bridge = "https://w0.peakpx.com/wallpaper/76/291/HD-wallpaper-enchanted-forest-art-forest-river-fantasy.jpg";
+
+const firefly = "https://aiartshop.com/cdn/shop/products/magical-forest-01-ai-painting-917.webp?v=1677621643";
+const fireflyTree = "https://www.bhmpics.com/downloads/enchanted-forest-desktop-wallpaper-/25.1582033499_enchanted-forest_vsthemes_ru-21.webp";
+const altFireflyTree = "https://e1.pxfuel.com/desktop-wallpaper/614/830/desktop-wallpaper-enchanted-forest-enchanted-forest-backgrounds-mystical-forest.jpg";
+
+const upstream = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0dce719a-cfab-4cbd-b5f0-6305626028e6/d6u1xef-2904418a-6874-44c6-a252-c06e7ec9bc6d.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBkY2U3MTlhLWNmYWItNGNiZC1iNWYwLTYzMDU2MjYwMjhlNlwvZDZ1MXhlZi0yOTA0NDE4YS02ODc0LTQ0YzYtYTI1Mi1jMDZlN2VjOWJjNmQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.1JFOYPHMUdvnp8uxEjXE3Mo7AvXll8mPq7m46oXZOEU";
+const waterfall = "https://w0.peakpx.com/wallpaper/700/796/HD-wallpaper-fantasy-forest-forest-colorful-lovely-view-colors-beautiful-magic-trees-swamp-valley-warrior-water-splendor-green-men.jpg";
+const treeHouse = "https://images.nightcafe.studio/jobs/wi0XlQDMXSgsQywQQ0FE/wi0XlQDMXSgsQywQQ0FE--2--g8ety.jpg?tr=w-1600,c-at_max";
+
+const downstream = "https://as1.ftcdn.net/v2/jpg/05/47/03/72/1000_F_547037240_LUekRoVId3q55j70Spyea0K6SdsJjzFo.jpg";
+const downstream2 = "https://as2.ftcdn.net/v2/jpg/05/73/05/69/1000_F_573056930_NCI3XfYl3vaJQKiqZXtTmPy77z7RGK8y.jpg";
+
+const swamp1 = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0975fc90-2a41-4206-9525-18338e368074/d966lr9-1f0cab17-f5df-4b5d-a95a-1ecd07806dcd.jpg/v1/fit/w_828,h_606,q_70,strp/swamp_by_adampaquette_d966lr9-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODA0IiwicGF0aCI6IlwvZlwvMDk3NWZjOTAtMmE0MS00MjA2LTk1MjUtMTgzMzhlMzY4MDc0XC9kOTY2bHI5LTFmMGNhYjE3LWY1ZGYtNGI1ZC1hOTVhLTFlY2QwNzgwNmRjZC5qcGciLCJ3aWR0aCI6Ijw9MTEwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.D2qN6t3k8SMJF2bjbg2DPJiqdaed_-TVpPKtap7wfOg";
+const swamp2 = "https://i.pinimg.com/originals/16/f2/77/16f277499b1b23f53372815889c15e00.jpg";
+const swampVillage = "https://e0.pxfuel.com/wallpapers/609/778/desktop-wallpaper-%E2%9C%BC-lanterns-in-the-swamp-%E2%9C%BC-lanterns-colors-scenery-trees-jungle-lamps-attractions-in-dreams-forests-paintings-swamp-beautiful-fireflies-creative-pre-made-love-four-seasons-landscapes.jpg";
+const swampCircle = "https://images.nightcafe.studio/jobs/8qWAPhnH1CBqk9pBaFh1/8qWAPhnH1CBqk9pBaFh1--2--9kc8q_2x.jpg?tr=w-1600,c-at_max";
+const swampRuins = "https://images.nightcafe.studio/jobs/pWlflDYguTfYUdbhhKjN/pWlflDYguTfYUdbhhKjN--1--jwcey_2x.jpg?tr=w-1600,c-at_max";
+
+
+// changeViewTo (portal)
